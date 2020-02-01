7391b741c7bd611c4c81e7e39afe5a58370a8649
risk08 <31163845+risk08@users.noreply.github.com>
2020-02-01 20:34:32 +0000

    cookies working

--- Switcher.py
+++ Switcher.py
@@ -1,22 +1,36 @@
-import gdata.youtube
-import gdata.youtube.service
+import csv
+import pickle
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-yt_service = gdata.youtube.service.YouTubeService()
+driver = webdriver.Chrome()
+driver.delete_all_cookies()
+driver.get("https://open.spotify.com/playlist/0dyWi0y1RHqZeRvRHlpoXo?si=QL4YUlhLT46mwbEfrw3u8g")
+for cookie in pickle.load(open("cookies.pkl", "rb")):
+     if 'expiry' in cookie:
+         del cookie['expiry']
+     driver.add_cookie(cookie)
+assert "Spotify" in driver.title
+playlist_title = driver.title[10:]
+no_of_songs = int(driver.find_element_by_class_name('TrackListHeader__text-silence.TrackListHeader__entity-additional-info').text[0:2])
+songs = []
 
-# Turn on HTTPS/SSL access.
-# Note: SSL is not available at this time for uploads.
-yt_service.ssl = True
+##for i in range(no_of_songs):
+##    song = []
+##    writer = csv.writer(file)
+##    writer.writerows(songs)
 
-yt_service.developer_key = 'AIzaSyCl4GSLMM5446DAF-9KVPBnULiIWldCsew'
-yt_service.client_id = '6546054084-0dhu8h54cknvsqcf7ea2cpog9cjl9ki1.apps.googleusercontent.com'
+driver.get("https://www.youtube.com")
+##pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
+try:
+    element = WebDriverWait(driver, 800).until(
+        EC.presence_of_element_located((By.ID, "avatar-btn"))
+    )
+    print("success")
+finally:
+    driver.quit()
 
-def GetAuthSubUrl():
-    next = 'http://www.example.com/video_upload.pyc'
-    scope = 'http://gdata.youtube.com'
-    secure = False
-    session = True
-    yt_service = gdata.youtube.service.YouTubeService()
-    return yt_service.GenerateAuthSubURL(next, scope, secure, session)
-
-authSubUrl = GetAuthSubUrl()
-print authSubUrl
+driver.quit()

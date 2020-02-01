5e2a9e8be18ce1607ec27437b045f7f1f712a436
risk08 <terrathorn25@gmail.com>
2020-02-01 12:02:55 +0000

    v1 complete

--- Switcher.py
+++ Switcher.py
@@ -1,13 +1,16 @@
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 driver.get("https://open.spotify.com/playlist/0dyWi0y1RHqZeRvRHlpoXo?si=QL4YUlhLT46mwbEfrw3u8g")
 assert "Spotify" in driver.title
 playlist_title = driver.title[10:]
 no_of_songs = int(driver.find_element_by_class_name('TrackListHeader__text-silence.TrackListHeader__entity-additional-info').text[0:2])
-songs = [["Song:","Artists:"]]
+songs = []
 
 for i in range(no_of_songs):
     song = []
@@ -23,5 +26,14 @@ print(songs)
 with open('playlist.csv','w',newline='') as file:
     writer = csv.writer(file)
     writer.writerows(songs)
+
+##driver.get("https://accounts.google.com/signin/v2/identifier?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den%26next%3D%252F&hl=en&ec=65620&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
+##try:
+##    element = WebDriverWait(driver, 60).until(
+##        EC.presence_of_element_located((By.ID, "avatar-btn"))
+##    )
+##    print("success")
+##finally:
+##    driver.quit()
 
 driver.quit()

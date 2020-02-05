47a6aa2683197f4968ddcc3d7e2b88a3795aca8c
ben <git@git.com>
2020-02-05 21:58:06 +0000

    playlist creation

--- Switcher.py
+++ Switcher.py
@@ -18,19 +18,61 @@ playlist_title = driver.title[10:]
 no_of_songs = int(driver.find_element_by_class_name('TrackListHeader__text-silence.TrackListHeader__entity-additional-info').text[0:2])
 songs = []
 
-##for i in range(no_of_songs):
-##    song = []
-##    writer = csv.writer(file)
-##    writer.writerows(songs)
+   
+for i in range(no_of_songs):
+    song = []
+    song_borders = driver.find_elements_by_class_name('track-name-wrapper.tracklist-top-align')[i]
+    song_title = song_borders.find_element_by_class_name('tracklist-name.ellipsis-one-line')
+    song_artists = song_borders.find_element_by_class_name('TrackListRow__artists.ellipsis-one-line')
+    song.append(song_title.text)
+    song.append(song_artists.text)
+    songs.append(song)
 
-driver.get("https://www.youtube.com")
-##pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
-try:
-    element = WebDriverWait(driver, 800).until(
-        EC.presence_of_element_located((By.ID, "avatar-btn"))
-    )
-    print("success")
-finally:
-    driver.quit()
+with open('playlist.csv','w',newline ='') as file:
+    writer = csv.writer(file)
+    writer.writerows(songs)
 
-driver.quit()
+
+
+driver.get("https://music.youtube.com/library/playlists")
+driver.implicitly_wait(1)
+click_element = driver.find_element_by_class_name('image.style-scope.ytmusic-two-row-item-renderer')
+driver.execute_script("arguments[0].click();", click_element)
+driver.implicitly_wait(1)
+# driver.execute_script("arguments[0].click();", click_element)
+# driver.execute_script("arguments[0].click();", click_element)
+# driver.implicitly_wait(5)
+input_element = driver.find_element_by_class_name('input.style-scope.ytmusic-playlist-form')
+input_element.send_keys(playlist_title)
+
+input_element = driver.find_element_by_class_name('description-input.input.style-scope.ytmusic-playlist-form')
+input_element.send_keys('Converted by Playlist Switcher')
+
+click_element = driver.find_element_by_class_name('style-scope.paper-dropdown-menu')
+driver.execute_script("arguments[0].click();", click_element)
+driver.implicitly_wait(1)
+
+click_element = driver.find_element_by_xpath("/html/body/ytmusic-dialog/paper-dialog-scrollable/div/div/ytmusic-playlist-form/paper-dropdown-menu/paper-menu-button/iron-dropdown/div/div/paper-listbox/paper-item[3]")
+driver.execute_script("arguments[0].click();", click_element)
+
+click_element = driver.find_element_by_class_name('submit-button.style-scope.ytmusic-playlist-form')
+driver.execute_script("arguments[0].click();", click_element)
+driver.implicitly_wait(1)
+
+for j in range(len(songs)):
+    click_element = driver.find_element_by_xpath('/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]/paper-icon-button[1]/iron-icon')
+    driver.execute_script("arguments[0].click();", click_element)
+    input_element = driver.find_element_by_xpath('/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[2]/ytmusic-search-box/div/div[1]/input')
+    input_element.clear()
+    input_element.send_keys(songs[j][0])
+    for n in range(len(songs[j])):
+        input_element.send_keys(" ")
+        input_element.send_keys(songs[j][n])
+    input_element.send_keys("\n")
+    driver.implicitly_wait(1)
+    
+# input_element = driver.find_element_by_class_name('style-scope.iron-autogrow-textarea')
+# input_element.send_keys('Convertion by Playlist Switcher')
+# pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
+
+# driver.quit()
